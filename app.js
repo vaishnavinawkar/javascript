f791a96a8458a5e1ac6e6973f84e7aef2cf218c4
vaishnavinawkar <vaishnavinawkar3101@gmail.com>
2021-09-03 20:18:22 +0530

    logical operators

--- app.js
+++ app.js
@@ -1,60 +1,90 @@
 //Arithmetic oprators
- const specific_name ="vaishnavi"
+//  const specific_name ="vaishnavi"
 
- console.log(specific_name)
+//  console.log(specific_name)
 
- const a=2
- const b=15
+//  const a=2
+//  const b=15
 
 
- console.log(a+b)
+//  console.log(a+b)
 
- console.log(a-b)
+//  console.log(a-b)
 
- console.log(a*b)
+//  console.log(a*b)
 
- console.log(a/b)
+//  console.log(a/b)
 
- console.log(a%b)
+//  console.log(a%b)
 
- console.log(a**b)
+//  console.log(a**b)
 
- const c=a+b
+//  const c=a+b
 
-let c=5
+// let c=5
 
-c**=3
+// c**=3
 
-//assignment oprators
+// //assignment oprators
 
-let name='vaishnavi'
+// let name='vaishnavi'
 
-let a =20
+// let a =20
 
-a+=name
+// a+=name
 
-a = a+1
+// a = a+1
 
-console.log(a)
-console.log(typeof a)
+// console.log(a)
+// console.log(typeof a)
 
-// compariosn operators
+// // compariosn operators
 
-const a= 21
-const b=7
-const c=21
+// const a= 21
+// const b=7
+// const c=21
 
-console.log('a>b', a>b)
-console.log('a<b', a<b)
-console.log('a==b', a==b)
-console.log('a==c', a==c)
-console.log('a>=c', a>=c)
+// console.log('a>b', a>b)
+// console.log('a<b', a<b)
+// console.log('a==b', a==b)
+// console.log('a==c', a==c)
+// console.log('a>=c', a>=c)
 
-console.log('-------------------------------------------------')
+// console.log('-------------------------------------------------')
 
-// -----------------------------------
- const e=11
- const d='11'
+// // -----------------------------------
+//  const e=11
+//  const d='11'
 
- console.log(e==d)
- console.log(e === d)
+//  console.log(e==d)
+//  console.log(e === d)
+
+ //e==d 
+
+ //logical operators
+
+ const g=2
+ const k=3
+
+ const j=35
+
+ console.log(j > g && j<k)
+
+ console.log('g==k', g==k)
+ console.log('g != k', g!=k)
+
+//  console.log('Truth table for AND')
+//  console.log('')
+
+//  console.log('false && false', false&&false)
+//  console.log('true && false', true&&false)
+//  console.log('false && true', false&&true)
+//  console.log('true && true', true&&true)
+
+//  console.log('Truth table for OR')
+//  console.log('')
+
+//  console.log('false || false', false||false)
+//  console.log('true || false', true||false)
+//  console.log('false || true', false||true)
+//  console.log('true || true', true||true)
