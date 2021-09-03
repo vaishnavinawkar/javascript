46e7d3e1696ad1a798da776d3745885084db80a8
vaishnavinawkar <vaishnavinawkar3101@gmail.com>
2021-09-03 19:39:11 +0530

    arithmetic

---
+++ app.js
@@ -0,0 +1,26 @@
+//Arithmetic oprators
+ const specific_name ="vaishnavi"
+
+ console.log(specific_name)
+
+ const a=2
+ const b=15
+
+
+ console.log(a+b)
+
+ console.log(a-b)
+
+ console.log(a*b)
+
+ console.log(a/b)
+
+ console.log(a%b)
+
+ console.log(a**b)
+
+ const c=a+b
+
+let c=5
+
+c**=3
