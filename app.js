6e0eb7341d7375739169798461edabde35e73848
vaishnavinawkar <vaishnavinawkar3101@gmail.com>
2021-09-03 20:05:52 +0530

    compariosn operators

--- app.js
+++ app.js
@@ -24,3 +24,37 @@
 let c=5
 
 c**=3
+
+//assignment oprators
+
+let name='vaishnavi'
+
+let a =20
+
+a+=name
+
+a = a+1
+
+console.log(a)
+console.log(typeof a)
+
+// compariosn operators
+
+const a= 21
+const b=7
+const c=21
+
+console.log('a>b', a>b)
+console.log('a<b', a<b)
+console.log('a==b', a==b)
+console.log('a==c', a==c)
+console.log('a>=c', a>=c)
+
+console.log('-------------------------------------------------')
+
+// -----------------------------------
+ const e=11
+ const d='11'
+
+ console.log(e==d)
+ console.log(e === d)
