8c36e45d1d8116ab0f755e721dfb3ad4465140bd
vaishnavinawkar <vaishnavinawkar3101@gmail.com>
2021-09-06 19:28:05 +0530

    switch case

--- app.js
+++ app.js
@@ -1,46 +1,86 @@
-// const rock=document.querySelector('#rock')
+const rock=document.querySelector('#rock')
 // console.log(rock)
 
-// const paper=document.querySelector('#paper')
+const paper=document.querySelector('#paper')
 // console.log(paper)
 
-// const Scissor=document.querySelector('#Scissor')
+const Scissor=document.querySelector('#scissor')
 // console.log(Scissor)
 
-function logName(name="Vaishnavi", age=20){
-    console.log(name + " your are " +   age )
+const board =document.querySelector("#board")
+
+// const handler =(action)=>{
+//     console.log("you clicked " + action)
+// }
+const handler =(event)=>{
+    const action =event.target.id
+    // if(action =='rock'){
+    //     board.innerHTML="Rock crushes scissors"
+    // } else if (action =='paper'){
+    //     board.innerHTML="paper traps ROck"
+    // }else if (action =='scissor') {
+        
+    //     board.innerHTML="scissor cuts paper"
+    // }else {
+    //     board.innerHTML="click something"
+    // }
+
+    switch (action){
+        case 'rock':
+            board.innerHTML="Rock crushes scissors"
+            break
+        case 'paper':
+            board.innerHTML="paper traps ROck"
+            break
+        case 'scissor':
+            board.innerHTML="scissor cuts paper"
+            break
+        default:
+            board.innerHTML="click something"
+
+    }
 }
+
+rock.addEventListener('click', handler)
+paper.addEventListener('click', handler)
+scissor.addEventListener('click', handler)
+// paper.addEventListener('click', handler('paper'))
+// scissor.addEventListener('click', handler('scissor'))
+
+// function logName(name="Vaishnavi", age=20){
+//     console.log(name + " your are " +   age )
+// }
 //never mispell the function keyword
 //a function can have multilple parameters
 //you can set default values for the parameters
 //anyvariable you defined in a function's body is local to that function only
-logName()
+// logName()
 
 
-function add(a=0,b=0){
-    const sum= a+b
-    console.log(a+b)
-}
-add(2,8)
+// function add(a=0,b=0){
+//     const sum= a+b
+//     console.log(a+b)
+// }
+// add(2,8)
 
 
-function Addition(a=0,b=0){
-    return a + b
-}
-const result = Addition(12,99)
-console.log(result)
+// function Addition(a=0,b=0){
+//     return a + b
+// }
+// const result = Addition(12,99)
+// console.log(result)
 
-//ECMA SCRIPT 
+// //ECMA SCRIPT 
 
-const sub=function (a=0,b=0){
-    return a - b
-}
+// const sub=function (a=0,b=0){
+//     return a - b
+// }
 
-console.log(sub(10,5))
+// console.log(sub(10,5))
 
-//by using arrow function delclartion
-const mul= (a=0,b=0)=> {
-    return a * b
-}
+// //by using arrow function delclartion
+// const mul= (a=0,b=0)=> {
+//     return a * b
+// }
 
-console.log(mul(10,5))
+// console.log(mul(10,5))
