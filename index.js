b23f9f220a5d2216d1155b327568bba03fe9341d
vaishnavinawkar <vaishnavinawkar3101@gmail.com>
2021-09-08 20:16:31 +0530

    ES6 arrow function

--- index.js
+++ index.js
@@ -32,3 +32,34 @@ console.log(obj.email)//without using ES6
 let {name,email,age,dum}= obj;
 console.log(email)
 console.log(dum)
+
+
+let person = {
+    firstname: "Nikhil",
+    lastname: "Agarwal",
+    age1: 20,
+    middlename: "DNA"
+}
+let {age1, firstname, lastname, middlename="" } = person;
+console.log(age1);
+console.log(firstname);
+console.log(lastname);
+console.log(middlename);
+
+
+//es6 arrow function
+//it just like a function but itjust a short expression to write a function 
+
+function add(x,y){ //traditional methd to write function
+    return x+y
+}
+
+let addition=function (r,s){ //this is not es6 arrow function it just a function in a variable format
+    return r+s;
+}
+
+console.log(add(1,2))
+
+let sum= (q,w)=> q+w;//this is a ES6 arrow function
+console.log(sum(10,12))
+
